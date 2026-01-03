d1562356505652fbedeab5eea50143d465ca638e
The Skip WTF <31883729+sechi2000@users.noreply.github.com>
2026-01-04 00:34:41 +0100

    n

--- forum/applications/core/interface/ckeditor/ckeditor/plugins/ipsctrlenter/plugin.js
+++
@@ -1,1 +0,0 @@
-﻿CKEDITOR.plugins.add("ipsctrlenter",{init:function(c){c.setKeystroke(CKEDITOR.CTRL+13,"ipsCtrlEnter");c.addCommand("ipsCtrlEnter",{exec:function(b){var a=$("."+b.id).closest("form").find('[data-role\x3d"primarySubmit"]');a.length||(a=$("."+b.id).closest("form").find('button[type\x3d"submit"]'));a.length?a.click():$("."+b.id).closest("form").submit()}})}});
